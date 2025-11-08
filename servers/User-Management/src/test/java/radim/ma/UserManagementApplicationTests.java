9f1b5b5ca9a4a7b95a05ae995845be3591c150a3
RadimYassin <radimyassin20@gmail.com>
2025-11-08 15:16:05 +0100

    feat(user-management): create user management service

---
+++ servers/User-Management/src/test/java/radim/ma/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package radim.ma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
