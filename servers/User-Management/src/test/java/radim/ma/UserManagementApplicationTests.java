9c78cb4891aff3383759bb8d424d311ef9ad9cff
RadimYassin <radimyassin20@gmail.com>
2025-12-17 01:53:15 +0100

    fix: mock JavaMailSender in User-Management tests
    - Add @MockBean annotation for JavaMailSender in test context
    - Resolves test failure where EmailServiceImpl requires JavaMailSender
    - Allows Spring Boot application context to load successfully in tests

--- servers/User-Management/src/test/java/radim/ma/UserManagementApplicationTests.java
+++ servers/User-Management/src/test/java/radim/ma/UserManagementApplicationTests.java
@@ -2,9 +2,14 @@ package radim.ma;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootTest
 class UserManagementApplicationTests {
+
+    @MockBean
+    private JavaMailSender javaMailSender;
 
     @Test
     void contextLoads() {
