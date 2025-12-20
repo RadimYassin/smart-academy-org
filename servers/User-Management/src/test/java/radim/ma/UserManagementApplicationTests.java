2ad735832f1c701e5f2ae431040cf52d707d3b71
RadimYassin <radimyassin20@gmail.com>
2025-12-20 23:08:53 +0100

    fix: resolve GitHub Actions test failure - Added application-test.yml - Fixed missing mail properties - Mocked RabbitTemplate for tests

--- servers/User-Management/src/test/java/radim/ma/UserManagementApplicationTests.java
+++ servers/User-Management/src/test/java/radim/ma/UserManagementApplicationTests.java
@@ -1,15 +1,21 @@
 package radim.ma;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserManagementApplicationTests {
 
     @MockBean
     private JavaMailSender javaMailSender;
+
+    @MockBean
+    private RabbitTemplate rabbitTemplate;
 
     @Test
     void contextLoads() {
